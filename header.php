c2ffd99a362ad032c00f9c21f37a215ee287a7d9
yutaarase <82860437+yutaarase@users.noreply.github.com>
2022-03-10 02:24:05 +0900

    Delete header.php

--- header.php
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script language="JavaScript">
-$(document).ready( function () {
-   $("a[href^='http']:not([href*='" + location.hostname + "'])").attr('target', '_blank');
-})
-</script>
